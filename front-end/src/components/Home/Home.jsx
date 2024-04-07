379c6088cd7bfa8789080badc44bc72420eb37b7
amaljithmk07 <amaljithmk123@gmail.com>
2024-04-07 16:33:08 +0530

    Initial commit

---
+++ front-end/src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import "./Home.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Home = () => {
+
+
+
+
+  
+  return (
+    <div>
+     
+     
+
+
+    </div>
+  );
+};
+
+export default Home;
