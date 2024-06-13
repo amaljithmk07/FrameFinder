79c28692b105c5ef3d6586aa2d0107492b39cb02
amaljithmk07 <amaljithmk123@gmail.com>
2024-06-13 10:33:50 +0530

    Previous booking displayed

--- front-end/src/components/Photographer/PhotoPreviousBooking.jsx
+++ front-end/src/components/Photographer/PhotoPreviousBooking.jsx
@@ -28,7 +28,34 @@ const PhotoPreviousBooking = () => {
   return (
     <div>
       <Toaster />
-      <div className="p-previous-booking-main-body"></div>
+      <div className="p-previous-booking-main-body">
+        <div className="p-previous-booking-description-body">
+          <div className="p-previous-booking-title">Client Bookings</div>
+          <div className="p-previous-booking-paragraph">
+            Welcome to the booking overview page. Below is a comprehensive list
+            of clients who have scheduled sessions with me. Each booking
+            includes detailed information about the session type, date, time,
+            and a personalized description. Click on each booking to learn more
+            about the specific requirements and expectations of each session. I
+            look forward to capturing these special moments and delivering
+            exceptional results for every client.
+          </div>
+        </div>
+
+        <div className="p-previous-booking-sec">
+          {bookings.map((data) => (
+            <div className="p-previous-booking-card-body">
+              <div>{data.name}</div>
+              <div>{data.email}</div>
+              <div>
+                {data.address},{data.city},{data.state}
+              </div>
+              <div>{data.phone}</div>
+              <button>Review</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
